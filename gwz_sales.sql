8fec0abb7a4fad84cca82bcbfba3a45e128d1ca4
moleroan <moleroan@gmail.com>
2024-11-11 15:21:00 +0100

    Update gwz_sales.sql
    turnover and purchase cost updated

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,5 +1,5 @@
-SELECT date_date,purchase_cost
-SUM (turnover) AS turnover
+SELECT date_date,ROUND(purchase_cost,2),
+ROUND(SUM (turnover),2) AS turnover
 FROM data-analytics-bootcamp-363212.course14.gwz_sales
 GROUP BY date_date
 ORDER BY date_date
